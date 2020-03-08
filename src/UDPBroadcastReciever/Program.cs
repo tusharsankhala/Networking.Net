f87ed4f3684d7a2daa2435e51585c085953cd582
tusharsankhala <tusharsankhala@gmail.com>
2020-03-08 18:58:16 +0530

    First pass of udp socket sender and reciever

---
+++ src/UDPBroadcastReciever/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPBroadcastReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sockBreaodCastReciever = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipeLocal = new IPEndPoint(IPAddress.Any, 23000);
+
+            byte[] recievedBuffer = new byte[512];
+            StringBuilder recievedBytes = new StringBuilder(1);
+
+            try
+            {
+                sockBreaodCastReciever.Bind(ipeLocal);
+                while (true)
+                {
+                    int dataByteRecieved = sockBreaodCastReciever.Receive(recievedBuffer);
+                    if (dataByteRecieved > 0)
+                    {
+
+                        recievedBytes.Append(Encoding.ASCII.GetString(recievedBuffer));
+                        
+                        Console.WriteLine("Number of Bytes recieved: " + dataByteRecieved);
+                        Console.WriteLine("Data is: " + recievedBytes.ToString());
+
+                        // Clear the buffer.
+                        Array.Clear(recievedBuffer, 0, recievedBuffer.Length);
+                        recievedBytes.Clear();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ src/UDPBroadcastSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPBroadcastSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sockBroadcaster = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sockBroadcaster.EnableBroadcast = true;
+
+            // End Point.    
+            IPEndPoint broadcastEP = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 23000);
+
+            // Data
+            byte[] broadcastBuffer = new byte[] { 0x0D, 0x0A }; // stands for /r/n.
+            string inputStr;
+
+            try
+            {
+                while (true)
+                {
+                    inputStr = Console.ReadLine();
+
+                    if (inputStr == "<EXIT>")
+                    {
+                        break;
+                    }
+
+                    broadcastBuffer = Encoding.ASCII.GetBytes(inputStr);
+                    sockBroadcaster.SendTo(broadcastBuffer, broadcastEP);
+                }
+                sockBroadcaster.Shutdown(SocketShutdown.Both);
+                sockBroadcaster.Close();
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+            }
+        }
+    }
+}
